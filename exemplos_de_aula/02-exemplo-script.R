98a38d880061688506fb7dafdb7466b05870bfbd
Beatriz Milz <beatriz.milz@hotmail.com>
2021-02-08 22:30:02 -0300

    aula 3 - alterações em aula

--- exemplos_de_aula/02-exemplo-script.R
+++ exemplos_de_aula/02-exemplo-script.R
@@ -8,15 +8,16 @@
 
 # Carregando os dados -----------------------------------------------------
 
-vetor_de_dados_experimentais <- c(3.37, 4.32, 5.67, 8.31, 9.21, 7.89, 2.13, 4.13, 1.23, 10.11)
+vetor_de_dados_experimentais <- c(3.37, 4.32, 5.67, 8.31, 9.21, 7.89, 2.13,
+                                  4.13, 100.23, 10.11)
 
 # Calculando estatísticas -------------------------------------------------
 
 media <- mean(vetor_de_dados_experimentais)
 
-desvio_padrao <- sd(vetor_de_dados_experimentais)
+desvio_padrao <- round(sd(vetor_de_dados_experimentais), 2)
 
-coeficiente_de_variacao <- 100*desvio_padrao/media
+coeficiente_de_variacao <- 100 * desvio_padrao / media
 
 maximo <- max(vetor_de_dados_experimentais)
 
@@ -24,12 +25,27 @@ minimo <- min(vetor_de_dados_experimentais)
 
 # Imprimindo os resultados finais -----------------------------------------
 
-mensagem_que_vou_imprimir <- paste0("A média dos dados é ", media, ", já o desvio padrão é ", desvio_padrao,
-                                    ". O coeficiente de variação, portanto, é ", coeficiente_de_variacao,
-                                    ", enquanto o máximo e o mínimo, por sua vez, são ", minimo, " e ", maximo,
-                                    ".")
+mensagem_que_vou_imprimir <-
+  paste0(
+    "A média dos dados é ",
+    media,
+    ", já o desvio padrão é ",
+    format(desvio_padrao,
+           decimal.mark = ","),
+    ". O coeficiente de variação, portanto, é ",
+    round(coeficiente_de_variacao, 2),
+    ", enquanto o máximo e o mínimo, por sua vez, são ",
+    minimo,
+    " e ",
+    maximo,
+    "."
+  )
 
-paste0(mensagem_que_vou_imprimir)
+#  Atalho para identar : ctrl + shift + A
+
+
+
+print(mensagem_que_vou_imprimir)
 # olhe o console para ver esses resultados
 
 # Exercício
